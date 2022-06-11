d1a68cc2ec80a28541beae0f69b4c2d6b500e5e8
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-06-11 16:11:20 -0400

    set up firebase

---
+++ src/firebase/firebase-setup.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "hydra-hacks.firebaseapp.com",
+  projectId: "hydra-hacks",
+  storageBucket: "hydra-hacks.appspot.com",
+  messagingSenderId: "202084530836",
+  appId: "1:202084530836:web:02e20dbfacadf87acf1281"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
