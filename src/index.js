3a845af47631db5966ee863c6d3d55f7e4dde3f3
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-06-12 00:53:56 -0400

    qna page word

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const theme = extendTheme({
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode("gray.200", "gray.200")(props),
+      },
+    }),
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
--- src/pages/qna.jsx
+++ src/pages/qna.jsx
@@ -1,5 +1,85 @@
-const QNA = () => {
-  <>hey</>
-}
+import { useState } from "react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  Link,
+  Avatar,
+  FormControl,
+  FormHelperText,
+  InputRightElement,
+  Grid,
+  GridItem,
+  Text,
+  Center,
+} from "@chakra-ui/react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import FutureView from "../svg/futureview.svg";
 
-export default QNA
+const BackArrow = chakra(FaArrowLeft);
+const ForwardArrow = chakra(FaArrowRight);
+
+const QNA = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  return (
+    <Box bgColor="gray.200">
+      <img style={{ paddingLeft: "15px" }} src={FutureView} />
+      <Flex
+        flexDirection="column"
+        width="100wh"
+        height="30vh"
+        backgroundColor="gray.200"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Stack flexDir="row" mb="2" justifyContent="center" alignItems="center">
+          <Box minW={{ md: "60px" }}>
+            <Stack p="1rem" mt="0" backgroundColor="gray.300" boxShadow="md">
+              <BackArrow size="30px" />
+            </Stack>
+          </Box>
+          <Box minW={{ base: "90%", md: "568px" }}>
+            <Stack p="1rem" backgroundColor="whiteAlpha.900" boxShadow="md">
+              <Center>
+                <Text fontSize="xl" fontWeight="bold">
+                  Why do you want to work here?
+                </Text>
+              </Center>
+            </Stack>
+          </Box>
+          <Box minW={{ md: "60px" }}>
+            <Stack p="1rem" backgroundColor="gray.300" boxShadow="md">
+              <ForwardArrow size="30px" />
+            </Stack>
+          </Box>
+        </Stack>
+        <Stack
+          flexDir="column"
+          mb="2"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box minW={{ base: "90%", md: "694px" }}>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+            ></Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default QNA;
